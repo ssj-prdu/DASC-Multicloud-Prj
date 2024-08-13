4fe68f28cbbf000f8334cc8ae8a0c20c259aadd5
jan2274 <jan2274@naver.com>
2024-08-13 19:42:02 +0900

    cloudfront

--- cloudfront.tf
+++ cloudfront.tf
@@ -1,53 +1,52 @@
-# ################## CloudFront Distribution ##################
-# resource "aws_cloudfront_distribution" "dasc-s3-web-distribution" {
-#   origin {
-#     domain_name = output.exam_site
-#     origin_id   = "S3-dasc-s3-web"
+locals {
+  s3_origin_id   = "dasc-s3-web-origin"
+  s3_domain_name = "dasc-s3-web.s3-website-dasc-s3-web.amazonaws.com"
+}
 
-#     custom_origin_config {
-#       http_port              = 80
-#       https_port             = 443
-#       origin_protocol_policy = "http-only"  # HTTP only로 설정
-#       origin_ssl_protocols   = ["TLSv1.2"]  # 필수 속성 추가
-#     }
-#   }
+resource "aws_cloudfront_distribution" "cloudfront" {
+  enabled = true
+  
+  origin {
+    origin_id                = local.s3_origin_id
+    domain_name              = local.s3_domain_name
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
 
+  default_cache_behavior {
+    
+    target_origin_id = local.s3_origin_id
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
 
-#   enabled             = true
-#   is_ipv6_enabled     = false
-#   comment             = "CloudFront distribution for S3 web hosting"
-#   default_root_object = "main.html"
+    forwarded_values {
+      query_string = true
 
-#   default_cache_behavior {
-#     allowed_methods        = ["GET", "HEAD"]
-#     cached_methods         = ["GET", "HEAD"]
-#     target_origin_id       = "S3-dasc-s3-web"
+      cookies {
+        forward = "all"
+      }
+    }
 
-#     forwarded_values {
-#       query_string = false
-#       cookies {
-#         forward = "none"
-#       }
-#     }
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+  }
 
-#     viewer_protocol_policy = "redirect-to-https"  # HTTP 요청을 HTTPS로 리디렉션
-#     min_ttl                = 0
-#     default_ttl            = 3600
-#     max_ttl                = 86400
-#     compress               = true  # 객체 자동 압축
-#   }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 
-#   restrictions {
-#     geo_restriction {
-#       restriction_type = "none"
-#     }
-#   }
-
-#   viewer_certificate {
-#     cloudfront_default_certificate = true
-#   }
-
-#   tags = {
-#     Name = "dasc-s3-web-distribution"
-#   }
-# }
+  price_class = "PriceClass_200"
+  
+}
