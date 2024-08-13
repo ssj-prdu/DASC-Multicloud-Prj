c3b52bd2cdc60991e676d2d7b45745a8a0b1d5d5
jan2274 <jan2274@naver.com>
2024-08-13 17:22:43 +0900

    “ㅔㅕ노”

--- s3.tf
+++ s3.tf
@@ -7,6 +7,11 @@ resource "aws_s3_bucket" "dasc-s3-web" {
   }
 }
 
+resource "aws_s3_bucket_acl" "dasc-s3-web-acl" {
+  bucket = aws_s3_bucket.dasc-s3-web.id
+  acl    = "public-read"
+}
+
 resource "aws_s3_bucket_website_configuration" "dasc-s3-web" {
   bucket = aws_s3_bucket.dasc-s3-web.id
 
@@ -19,19 +24,19 @@ resource "aws_s3_bucket_website_configuration" "dasc-s3-web" {
   }
 }
 
-resource "aws_s3_bucket_policy" "dasc-s3-web-policy" {
-  bucket = aws_s3_bucket.dasc-s3-web.id
+# resource "aws_s3_bucket_policy" "dasc-s3-web-policy" {
+#   bucket = aws_s3_bucket.dasc-s3-web.id
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject",
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = "s3:GetObject",
-        Resource  = "arn:aws:s3:::dasc-s3-web/*"
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Sid       = "PublicReadGetObject",
+#         Effect    = "Allow",
+#         Principal = "*",
+#         Action    = "s3:GetObject",
+#         Resource  = "arn:aws:s3:::dasc-s3-web/*"
+#       }
+#     ]
+#   })
+# }
