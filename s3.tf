e3cd1add53a014bdc3bcb68150fe8a9c25329a6c
jan2274 <jan2274@naver.com>
2024-08-13 16:25:14 +0900

    “push”

--- s3.tf
+++ s3.tf
@@ -22,19 +22,3 @@ resource "aws_s3_bucket_website_configuration" "dasc-s3-web" {
 }
 
 
-# S3 버킷 정책 - 퍼블릭으로 읽기 가능하도록 설정
-resource "aws_s3_bucket_policy" "dasc-s3-web" {
-  bucket = aws_s3_bucket.dasc-s3-web.id
-
-  policy = jsonencode({
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::dasc-s3-web/*"
-    }
-  ]
-})
-}
