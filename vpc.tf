2b038b94721c3ee8761626ecfa09c43b0a22fb71
jan2274 <jan2274@naver.com>
2024-08-13 14:47:58 +0900

    “push”

--- vpc.tf
+++ vpc.tf
@@ -53,7 +53,7 @@ resource "aws_route_table" "dasc-rt-public" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.dasc-igw-main.id
+    gateway_id = aws_internet_gateway.aws-igw-main.id
   }
 
   tags = {
