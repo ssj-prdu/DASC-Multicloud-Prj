8030ab5c35dd52bdfc1e3358697189608d77f0a0
jan2274 <jan2274@naver.com>
2024-08-13 17:11:09 +0900

    “push”

--- s3.tf
+++ s3.tf
@@ -19,3 +19,20 @@ resource "aws_s3_bucket_website_configuration" "dasc-s3-web" {
     key = "error.html"  # 오류 시 표시할 문서 설정
   }
 }
+
+resource "aws_s3_bucket_policy" "dasc_s3_web_policy" {
+  bucket = aws_s3_bucket.dasc_s3_web.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.dasc_s3_web.bucket}/*"
+      }
+    ]
+  })
+}
