2aa2352d8fe726b96c32d44e8747fac520475cc5
jan2274 <jan2274@naver.com>
2024-08-13 20:43:16 +0900

    route53-acm

--- route53.tf
+++ route53.tf
@@ -11,5 +11,5 @@ resource "aws_route53_record" "ns" {
   name    = "ns.chjy0623.shop"  # 생성할 서브 도메인 이름
   type    = "CNAME"
   ttl     = 300
-  records = [output.cloudfront_dns]  
+  records = ["output.cloudfront_dns"]  
 }
