036722ac82fc53c75d7f2b64b60289f9613ddac8
jan2274 <jan2274@naver.com>
2024-08-13 16:59:21 +0900

    “push”

--- s3.tf
+++ s3.tf
@@ -1,16 +1,11 @@
 ################## Web hosting S3 ###############
 resource "aws_s3_bucket" "dasc-s3-web" {
   bucket = "dasc-s3-web"  # 고유한 S3 버킷 이름으로 변경하세요.
+  acl    = "public-read"
 
   tags = {
     Name        = "dasc-s3-web"
   }
-}
-
-# S3 버킷의 ACL 설정
-resource "aws_s3_bucket_acl" "dasc-s3-web-acl" {
-  bucket = aws_s3_bucket.dasc-s3-web.id
-  acl    = "public-read"
 }
 
 resource "aws_s3_bucket_website_configuration" "dasc-s3-web" {
