39b66194a3a301e6ec2b0549ff5f57928a5158a6
jan2274 <jan2274@naver.com>
2024-08-13 21:25:28 +0900

    route53-acm

--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
-output "bucket_endpoint" {
-  value = aws_s3_bucket_website_configuration.exam_site.website_endpoint
-  description = "The website endpoint URL for the S3 bucket"
-}
+# output "bucket_endpoint" {
+#   value = aws_s3_bucket_website_configuration.exam_site.website_endpoint
+#   description = "The website endpoint URL for the S3 bucket"
+# }
 
-output "cloudfront_endpoint" {
-    value = aws_cloudfront_distribution.cloudfront.domain_name
-}
+# output "cloudfront_endpoint" {
+#     value = aws_cloudfront_distribution.cloudfront.domain_name
+# }
