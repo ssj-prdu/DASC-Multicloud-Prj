02167980f627afd3dd42abe138abfb87f35a8233
jan2274 <jan2274@naver.com>
2024-08-13 17:12:54 +0900

    “push”

--- s3.tf
+++ s3.tf
@@ -1,7 +1,6 @@
 ################## Web hosting S3 ###############
 resource "aws_s3_bucket" "dasc-s3-web" {
   bucket = "dasc-s3-web"  # 고유한 S3 버킷 이름으로 변경하세요.
-  acl    = "public-read"
 
   tags = {
     Name = "dasc-s3-web"
@@ -18,21 +17,4 @@ resource "aws_s3_bucket_website_configuration" "dasc-s3-web" {
   error_document {
     key = "error.html"  # 오류 시 표시할 문서 설정
   }
-}
-
-resource "aws_s3_bucket_policy" "dasc_s3_web_policy" {
-  bucket = aws_s3_bucket.dasc_s3_web.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject",
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = "s3:GetObject",
-        Resource  = "arn:aws:s3:::dasc_s3_web/*"
-      }
-    ]
-  })
 }
