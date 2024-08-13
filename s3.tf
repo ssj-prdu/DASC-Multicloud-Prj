ea69a273d29158a195587572cb41fe79e2577476
jan2274 <jan2274@naver.com>
2024-08-13 16:01:10 +0900

    “push”

--- s3.tf
+++ s3.tf
@@ -1,21 +1,27 @@
 ################## Web hosting S3 ###############
-resource "aws_s3_bucket" "dasc-s3-web" {
-  bucket = "dasc-s3-web"  # 버킷 이름을 고유하게 설정하세요.
-
-  # 웹 호스팅을 위한 설정
-  website {
-    index_document = "main.html"  # 기본 문서
-    error_document = "error.html"  # 오류 시 표시할 문서
-  }
+resource "aws_s3_bucket" "dasc_s3_web" {
+  bucket = "dasc-s3-web"  # 고유한 S3 버킷 이름으로 변경하세요.
 
   tags = {
-    Name = "dasc-s3-web"
+    Name        = "dasc-s3-web"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "dasc_s3_web" {
+  bucket = aws_s3_bucket.dasc_s3_web.id
+
+  index_document {
+    suffix = "main.html"  # 기본 문서 설정
+  }
+
+  error_document {
+    key = "error.html"  # 오류 시 표시할 문서 설정
   }
 }
 
 # S3 버킷 정책 - 퍼블릭으로 읽기 가능하도록 설정
-resource "aws_s3_bucket_policy" "dasc-s3-web" {
-  bucket = aws_s3_bucket.dasc-s3-web.id
+resource "aws_s3_bucket_policy" "dasc_s3_web" {
+  bucket = aws_s3_bucket.dasc_s3_web.id
 
   policy = jsonencode({
     Version = "2012-10-17",
@@ -24,7 +30,7 @@ resource "aws_s3_bucket_policy" "dasc-s3-web" {
         Effect = "Allow",
         Principal = "*",
         Action = "s3:GetObject",
-        Resource = "${aws_s3_bucket.dasc-s3-web.arn}/*"
+        Resource = "${aws_s3_bucket.dasc_s3_web.arn}/*"
       }
     ]
   })
