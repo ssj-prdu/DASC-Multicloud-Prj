00b4ec387c0bfabbf5e436e28794d1b7e52b790b
jan2274 <jan2274@naver.com>
2024-08-14 03:13:43 +0900

    lambda,apigateway

--- aws/apigw.tf
+++ aws/apigw.tf
@@ -29,3 +29,12 @@ resource "aws_api_gateway_method" "options_method" {
   http_method   = "OPTIONS"
   authorization = "NONE"
 }
+################## Lambda 통합 설정 ##################
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
+  resource_id             = aws_api_gateway_resource.rest_resource.id
+  http_method             = aws_api_gateway_method.post_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda-exam.invoke_arn
+}
---
+++ aws/lambda.tf
@@ -0,0 +1,50 @@
+################## Lambda 함수 생성 ##################
+resource "aws_lambda_function" "lambda-exam" {
+  function_name = "dasc-lambda-check-examid"
+  handler       = "index.handler"
+  runtime       = "Python 3.9"
+
+  role = aws_iam_role.dasc-role-lambda-examid.arn
+
+  filename = "pymysql_layer.zip"  # 이 파일은 Lambda 함수 코드가 포함된 zip 파일입니다.
+
+  source_code_hash = filebase64sha256("pymysql_layer.zip")
+}
+
+################## Lambda 역할 설정 ##################
+resource "aws_iam_role" "dasc-role-lambda-examid" {
+  name = "dasc-role-lambda-examid"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = aws_iam_role.dasc-role-lambda-examid.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+################## AmazonEC2FullAccess 정책 부여 ##################
+resource "aws_iam_role_policy_attachment" "lambda_ec2_fullaccess_attachment" {
+  role       = aws_iam_role.dasc_role_lambda_examid.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+################## AmazonRDSFullAccess 정책 부여 ##################
+resource "aws_iam_role_policy_attachment" "lambda_rds_fullaccess_attachment" {
+  role       = aws_iam_role.dasc_role_lambda_examid.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+}
+################## AmazonAPIFullAccess 정책 부여 ##################
+resource "aws_iam_role_policy_attachment" "lambda_api_fullaccess_attachment" {
+  role       = aws_iam_role.dasc_role_lambda_examid.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonAPIFullAccess"
+}
