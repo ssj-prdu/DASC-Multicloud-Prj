ca1cb8976cedcd592391ba26524874f945e50b53
jan2274 <jan2274@naver.com>
2024-08-13 16:34:21 +0900

    “push”

--- s3.tf
+++ s3.tf
@@ -1,24 +1,3 @@
 ################## Web hosting S3 ###############
-resource "aws_s3_bucket" "dasc-s3-web" {
-  bucket = "dasc-s3-web"  # 고유한 S3 버킷 이름으로 변경
-
-  tags = {
-    Name        = "dasc-s3-web"
-  }
-}
-
-resource "aws_s3_bucket_website_configuration" "dasc-s3-web" {
-  bucket = aws_s3_bucket.dasc-s3-web.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-
-  depends_on = [aws_s3_bucket.dasc-s3-web]  # 의존성 명시
-}
 
 
