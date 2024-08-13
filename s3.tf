770f1c563173b7b001edb3a493ea2ca4523b39f6
jan2274 <jan2274@naver.com>
2024-08-13 16:20:35 +0900

    “push”

--- s3.tf
+++ s3.tf
@@ -1,37 +1,41 @@
 ################## Web hosting S3 ###############
-resource "aws_s3_bucket" "dasc-s3-web" {
-  bucket = "dasc-s3-web"  # 고유한 S3 버킷 이름으로 변경하세요.
+resource "aws_s3_bucket" "dasc_s3_web" {
+  bucket = "dasc-s3-web"  # 고유한 S3 버킷 이름으로 변경
 
   tags = {
     Name        = "dasc-s3-web"
+    Environment = "Production"
   }
 }
 
-resource "aws_s3_bucket_website_configuration" "dasc-s3-web" {
-  bucket = aws_s3_bucket.dasc-s3-web.id
+resource "aws_s3_bucket_website_configuration" "dasc_s3_web" {
+  bucket = aws_s3_bucket.dasc_s3_web.id
 
   index_document {
-    suffix = "main.html"  # 기본 문서 설정
+    suffix = "index.html"
   }
 
   error_document {
-    key = "error.html"  # 오류 시 표시할 문서 설정
+    key = "error.html"
   }
+
+  depends_on = [aws_s3_bucket.dasc_s3_web]  # 의존성 명시
 }
+
 
 # S3 버킷 정책 - 퍼블릭으로 읽기 가능하도록 설정
 resource "aws_s3_bucket_policy" "dasc-s3-web" {
   bucket = aws_s3_bucket.dasc-s3-web.id
 
   policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = "*",
-        Action = "s3:GetObject",
-        Resource = "${aws_s3_bucket.dasc-s3-web.arn}/*"
-      }
-    ]
-  })
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::dasc-s3-web/*"
+    }
+  ]
+})
 }
