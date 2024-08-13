3e6d4e70a67fe5a7ba9e8310b038d6b8610c6da4
jan2274 <jan2274@naver.com>
2024-08-13 19:23:51 +0900

    cloudfront

---
+++ s3-test.tf
@@ -0,0 +1,39 @@
+resource "aws_s3_bucket" "bucket_test" {
+  bucket = jhtest-bucket_test
+}
+
+resource "aws_s3_bucket_acl" "bucket_test" {
+  bucket = aws_s3_bucket.bucket_test.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_website_configuration" "bucket_test" {
+  bucket = aws_s3_bucket.bucket_test.id
+
+  index_document {
+    suffix = "main.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+}
+
+resource "aws_s3_bucket_policy" "bucket_test" {
+  bucket = aws_s3_bucket.bucket_test.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "AllowGetObjects"
+    Statement = [
+      {
+        Sid       = "AllowPublic"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.bucket_test.arn}/**"
+      }
+    ]
+  })
+}
