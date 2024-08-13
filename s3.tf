30e71f88f6ed314212a0062cc11908d2cf622231
jan2274 <jan2274@naver.com>
2024-08-13 17:17:55 +0900

    “ㅔㅕ노”

--- s3.tf
+++ s3.tf
@@ -18,3 +18,20 @@ resource "aws_s3_bucket_website_configuration" "dasc-s3-web" {
     key = "error.html"  # 오류 시 표시할 문서 설정
   }
 }
+
+resource "aws_s3_bucket_policy" "dasc-s3-web-policy" {
+  bucket = aws_s3_bucket.dasc-s3-web.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "arn:aws:s3:::dasc-s3-web/*"
+      }
+    ]
+  })
+}
