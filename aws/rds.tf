a99600ca610982525943a0201fd85e2d53467079
jan2274 <jan2274@naver.com>
2024-08-14 16:56:42 +0900

    edit rds 1656

--- aws/rds.tf
+++ aws/rds.tf
@@ -8,16 +8,30 @@ resource "aws_db_subnet_group" "dasc_db_subnet_group" {
   }
 }
 
+resource "aws_db_parameter_group" "dasc_db_param_group" {
+  name   = "dasc-dev-test-param-group"
+  family = "mysql8.0"  # MySQL 버전에 따라 수정 필요
+  description = "Parameter group for Dev/Test environments"
+
+  tags = {
+    Name = "dasc-dev-test-param-group"
+  }
+}
+
 resource "aws_db_instance" "dasc_rds" {
-  identifier           = "dasc-db"
+  identifier           = "dasc-rds"
   engine               = "mysql"
   instance_class       = "db.t3.micro"
   allocated_storage    = 20
-  db_name              = "mydatabase"
+  db_name              = "exam_system"
   username             = "admin"
-  password             = "password123"
+  password             = var.db_password
   db_subnet_group_name = aws_db_subnet_group.dasc_db_subnet_group.name
   vpc_security_group_ids = [aws_security_group.dasc-sg-rds.id]  # 관련된 보안 그룹 참조
+  multi_az             = true  # Multi-AZ 배포 활성화
+  monitoring_interval  = 60    # Enhanced Monitoring을 60초 간격으로 활성화
+  monitoring_role_arn  = aws_iam_role.rds_monitoring_role.arn  # 모니터링을 위한 IAM 역할
+  parameter_group_name = aws_db_parameter_group.dasc_db_param_group.name  # 파라미터 그룹 참조
   skip_final_snapshot  = true
 
   tags = {
