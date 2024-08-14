9bcc923a3f3820eb4281874cdafb443d96e7dbe1
jan2274 <jan2274@naver.com>
2024-08-14 16:37:13 +0900

    create rds 1637

--- aws/rds.tf
+++ aws/rds.tf
@@ -1,15 +1,24 @@
+resource "aws_db_subnet_group" "dasc_db_subnet_group" {
+  name       = "dasc-db-subnet-group"
+  description = "Subnet group for RDS instances"
+  subnet_ids = aws_subnet.dasc-subnet-private[*].id  # 프라이빗 서브넷의 ID를 모두 포함
+
+  tags = {
+    Name = "dasc-db-subnet-group"
+  }
+}
+
 resource "aws_db_instance" "dasc_rds" {
-  allocated_storage    = 20
+  identifier           = "dasc-db"
   engine               = "mysql"
-  engine_version       = "8.0"
-  instance_class       = "db.t2.micro"
+  instance_class       = "db.t3.micro"
+  allocated_storage    = 20
+  db_name              = "mydatabase"
   username             = "admin"
-  password             = var.db_password
-  parameter_group_name = "default.mysql8.0"
+  password             = "password123"
+  db_subnet_group_name = aws_db_subnet_group.dasc_db_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.mysql_sg.id]  # 관련된 보안 그룹 참조
   skip_final_snapshot  = true
-  publicly_accessible  = false
-  db_subnet_group_name = ""  # 이미 존재하는 DB 서브넷 그룹 이름을 사용
-  vpc_security_group_ids = ["sg-xxxxxxxx"]    # 기존의 VPC 보안 그룹 ID를 사용
 
   tags = {
     Name = "dasc-rds"
