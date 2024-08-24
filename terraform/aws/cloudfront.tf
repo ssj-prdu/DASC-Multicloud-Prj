62d34eb3308c240482e649637ffc06b23c4bffa9
jan2274 <jan2274@naver.com>
2024-08-24 17:17:03 +0900

    create 0824 1716

--- terraform/aws/cloudfront.tf
+++ terraform/aws/cloudfront.tf
@@ -1,52 +1,52 @@
-locals {
-  s3_origin_id   = "dasc-s3-web-origin"
-  s3_domain_name = "dasc-s3-web.s3-website.ap-northeast-2.amazonaws.com"
-}
+# locals {
+#   s3_origin_id   = "dasc-s3-web-origin"
+#   s3_domain_name = "dasc-s3-web.s3-website.ap-northeast-2.amazonaws.com"
+# }
 
-resource "aws_cloudfront_distribution" "cloudfront" {
-  enabled = true
+# resource "aws_cloudfront_distribution" "cloudfront" {
+#   enabled = true
   
-  origin {
-    origin_id                = local.s3_origin_id
-    domain_name              = local.s3_domain_name
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1.2"]
-    }
-  }
+#   origin {
+#     origin_id                = local.s3_origin_id
+#     domain_name              = local.s3_domain_name
+#     custom_origin_config {
+#       http_port              = 80
+#       https_port             = 443
+#       origin_protocol_policy = "http-only"
+#       origin_ssl_protocols   = ["TLSv1.2"]
+#     }
+#   }
 
-  default_cache_behavior {
+#   default_cache_behavior {
     
-    target_origin_id = local.s3_origin_id
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
+#     target_origin_id = local.s3_origin_id
+#     allowed_methods  = ["GET", "HEAD"]
+#     cached_methods   = ["GET", "HEAD"]
 
-    forwarded_values {
-      query_string = true
+#     forwarded_values {
+#       query_string = true
 
-      cookies {
-        forward = "all"
-      }
-    }
+#       cookies {
+#         forward = "all"
+#       }
+#     }
 
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 0
-    max_ttl                = 0
-  }
+#     viewer_protocol_policy = "redirect-to-https"
+#     min_ttl                = 0
+#     default_ttl            = 0
+#     max_ttl                = 0
+#   }
 
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
+#   restrictions {
+#     geo_restriction {
+#       restriction_type = "none"
+#     }
+#   }
   
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
+#   viewer_certificate {
+#     cloudfront_default_certificate = true
+#   }
 
-  price_class = "PriceClass_200"
+#   price_class = "PriceClass_200"
   
-}
+# }
