9debf0cf49100c2484e7b7980a4621f9d92912ee
jan2274 <jan2274@naver.com>
2024-09-05 16:52:05 +0900

    “gke-2”

--- terraform/gcp/vpc.tf
+++ terraform/gcp/vpc.tf
@@ -8,4 +8,14 @@ resource "google_compute_subnetwork" "dasc-subnet-main" {
   network = google_compute_network.dasc_vpc_main_network.id
   region = var.region
   ip_cidr_range = "192.168.128.0/23"
+
+    secondary_ip_range {
+    range_name    = "pods"
+    ip_cidr_range = "10.4.0.0/14"
+  }
+
+  secondary_ip_range {
+    range_name    = "services"
+    ip_cidr_range = "10.8.0.0/20"
+  }
 }
