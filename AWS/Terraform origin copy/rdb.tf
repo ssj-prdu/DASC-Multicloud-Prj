32b2259d2fb9644affa132c805ac1bea7bd49d3f
jan2274 <jan2274@naver.com>
2024-09-11 15:16:03 +0900

    move file

--- AWS/Terraform origin copy/rdb.tf
+++ AWS/Terraform origin copy/rdb.tf
@@ -1,9 +1,9 @@
-module "rdb" {
-  source = "./rdb_infra"
-  prefix = "dasc"
+# module "rdb" {
+#   source = "./rdb_infra"
+#   prefix = "dasc"
 
 
-  subnet_ids             = aws_subnet.dasc-subnet-private[*].id
-  vpc_security_group_ids = [aws_security_group.dasc-sg-rds.id]
-  db_password            = var.db_password
-}
+#   subnet_ids             = aws_subnet.dasc-subnet-private[*].id
+#   vpc_security_group_ids = [aws_security_group.dasc-sg-rds.id]
+#   db_password            = var.db_password
+# }
