e30413f1f1c6fab3654c89bd9f7e99428e80286d
jan2274 <jan2274@naver.com>
2024-09-11 15:12:53 +0900

    move file

--- AWS/Terraform copy/lambda_infra/output.tf
+++
@@ -1,3 +0,0 @@
-output "lambda_invoke_arn" {
-  value = aws_lambda_function.lambda-exam.invoke_arn
-}
--- AWS/Terraform copy/rdb.tf
+++
@@ -1,9 +0,0 @@
-module "rdb" {
-  source = "./rdb_infra"
-  prefix = "dasc"
-
-
-  subnet_ids             = aws_subnet.dasc-subnet-private[*].id
-  vpc_security_group_ids = [aws_security_group.dasc-sg-rds.id]
-  db_password            = var.db_password
-}
